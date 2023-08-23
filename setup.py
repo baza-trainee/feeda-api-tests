bf4b1dfff4a8383f940cff82b8ffb4403caa8eb8
Dmytro <dima.tischenko2012@gmail.com>
2023-08-23 21:46:36 +0300

    added auth header to logout

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="Feeda API tests",
     version="0.1.0",
-    authors=["Oleksandr Kozlov", "Dmytro Tyshchenko"],
+    author="Oleksandr Kozlov and Dmytro Tyshchenko",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
